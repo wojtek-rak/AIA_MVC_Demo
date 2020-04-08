f658a4e1030ece26a038daf9ff2f343335104d3f
Wojciech Rak <rak.wojtek@wp.pl>
2020-04-08 17:48:42 +0200

    Initial commit

---
+++ AIA_MVC_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AIA_MVC_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AIA_MVC_Demo
+ 
